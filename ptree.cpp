2d7050a8f44a5f623b7f114cea91d7bf3397ec05
MAY04 <nguyennhatnam11022003@gmail.com>
2020-10-30 10:21:40 +0700

    201030 102100

---
+++ ptree.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define pii pair<int, int> 
+#define lint long long
+#define ld long double
+#define TASK "ptree"
+
+int F(int u, int par, int x) 
+{
+
+}
+
+void driver()
+{
+    freopen(TASK".inp","r",stdin);
+    freopen(TASK".out","w",stdout);
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    driver();
+}
